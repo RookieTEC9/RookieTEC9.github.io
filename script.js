816383f4d356897caef48564de80dc2577be8e8c
RookieTEC9 <jeremiur.ps101q@gmail.com>
2016-05-09 20:27:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+//makes the copyright thingy
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() +  " RookieTEC9 Industries. All rights reserved.</p>";
+//navigator "nav01"
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='Index.htm'>Home + Subjects</a></li>" +
+"<li><a href='Grades.htm'>Grades/Projects </a></li>" +
+"<li><a href='Contactus.htm'>Contact us! </a></li>" +
+"<li><a href= 'About.html'>About </a></li>" +
+
+"</ul>"
