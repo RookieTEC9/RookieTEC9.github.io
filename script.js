3e722fd4186379f9fa8f61e00b59872de2288eac
RookieTEC9 <jeremiur.ps101q@gmail.com>
2016-05-09 20:34:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ document.getElementById("foot01").innerHTML =
 //navigator "nav01"
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='Index.htm'>Home + Subjects</a></li>" +
-"<li><a href='Grades.htm'>Grades/Projects </a></li>" +
-"<li><a href='Contactus.htm'>Contact us! </a></li>" +
-"<li><a href= 'About.html'>About </a></li>" +
+"<li><a href='Index.htm'>Home</a></li>" +
+"<li><a href='Grades.htm'>Videos</a></li>" +
+"<li><a href='Contactus.htm'>Transcriptions</a></li>" +
+"<li><a href= 'About.html'>About</a></li>" +
 
 "</ul>"
