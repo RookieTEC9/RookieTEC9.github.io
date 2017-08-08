1b52bd5a2eddaa0c04ecca1fc13d1158e09eac78
RookieTEC9 <jeremiur.ps101q@gmail.com>
2017-08-08 16:57:14 -0400

    Lol justin

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//makes the copyright thingy
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() +  " RookieTEC9 Industries. All rights reserved. <br> Photo used from Microsoft. <br> Some code used with permission from the orginal authors </p>";
+//navigator "nav01"
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href= 'About.html'>About</a></li>" +
+"<li><a href='EnderPlugin.html'>Plugin</a><li>" +
+"<li><a href='https://github.com/RookieTEC9'>GitHub</a>" +
+"<li><a href='https://bukkit.org/members/theendercrafter9.91183076/'>Bukkit</a>" +
+"<li><a href='http://gaming.stackexchange.com/users/125383/rookietec9'>Arqade</a>"
+"</ul>"
